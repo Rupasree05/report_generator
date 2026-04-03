89532048de21a28028a48f6e84986e8f2273a326
rupasreesaladi <rupasreesaladi@gmail.com>
2026-04-03 14:08:11 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,11 +1,11 @@
 import os
 import json
+import streamlit as st
 from groq import Groq
 from dotenv import load_dotenv
 
-load_dotenv()
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+api_key = st.secrets["GROQ_API_KEY"]
 MODEL = "llama-3.1-8b-instant"
 
 
